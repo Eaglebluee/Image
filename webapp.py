522e6f34f7a2c63a94938f264b751ef95b95749f
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-13 17:39:57 +0300

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,23 +1,27 @@
 import cv2
 import streamlit as st
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from streamlit_image_comparison import image_comparison
 import io
+
 
 def brighten_image(image, amount):
     img_bright = cv2.convertScaleAbs(image, beta=amount)
     return img_bright
 
+
 def blur_image(image, amount):
     blur_img = cv2.GaussianBlur(image, (11, 11), amount)
     return blur_img
+
 
 def enhance_details(img):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     hdr = cv2.detailEnhance(img_rgb, sigma_s=12, sigma_r=0.15)
     hdr_bgr = cv2.cvtColor(hdr, cv2.COLOR_RGB2BGR)
     return hdr_bgr
+
 
 def cartoon_effect(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -27,9 +31,11 @@ def cartoon_effect(img):
     cartoon_img = cv2.bitwise_and(color, color, mask=edges)
     return cartoon_img
 
+
 def greyscale(img):
     greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return greyscale
+
 
 def sepia(img):
     img_sepia = np.array(img, dtype=np.float64)
@@ -40,13 +46,16 @@ def sepia(img):
     img_sepia = np.array(img_sepia, dtype=np.uint8)
     return img_sepia
 
+
 def pencil_sketch_grey(img):
     sk_gray, sk_color = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.1)
     return sk_gray
 
+
 def invert(img):
     inv = cv2.bitwise_not(img)
     return inv
+
 
 def summer(img):
     summer_img = img.copy()
@@ -54,11 +63,13 @@ def summer(img):
     summer_img[..., 2] = np.clip(img[..., 2] * 0.8, 0, 255)
     return summer_img
 
+
 def winter(img):
     winter_img = img.copy()
     winter_img[..., 0] = np.clip(img[..., 0] * 0.8, 0, 255)
     winter_img[..., 2] = np.clip(img[..., 2] * 1.2, 0, 255)
     return winter_img
+
 
 def detect_faces(img):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -77,21 +88,30 @@ def detect_faces(img):
 
     return img
 
-def add_text(img, text, position, font_size=20, font_color=(255, 255, 255), font_thickness=2):
+
+def add_text(img, text, position, font_size=20, font_color=(255, 255, 255), font_thickness=2, font_path='Arial.ttf',
+             text_position='bottom'):
     # Convert PIL Image to OpenCV format
     img = np.array(img)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
     # Add text to the image
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text_size, _ = cv2.getTextSize(text, font, font_size, font_thickness)
-    text_position = (position[0] - text_size[0] // 2, position[1] + text_size[1] // 2)
-    cv2.putText(img, text, text_position, font, font_size, font_color, font_thickness)
+    font = ImageFont.truetype(font_path, font_size)
+    draw = ImageDraw.Draw(img)
+    text_size = draw.textsize(text, font=font)
+
+    if text_position == 'top':
+        text_position = (position[0] - text_size[0] // 2, position[1] - text_size[1] // 2)
+    elif text_position == 'bottom':
+        text_position = (position[0] - text_size[0] // 2, position[1] + text_size[1] // 2)
+
+    draw.text(text_position, text, font=font, fill=font_color, stroke_width=font_thickness, stroke_fill=font_color)
 
     # Convert back to PIL Image format
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = Image.fromarray(img)
     return img
+
 
 def main_loop():
     st.sidebar.title("Filter Options")
@@ -112,17 +132,19 @@ def main_loop():
     filter_tooltip = filters[selected_filter]
     st.sidebar.text(filter_tooltip)
 
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-
     brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
     apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
 
     add_text_checkbox = st.sidebar.checkbox("Add Text")
     if add_text_checkbox:
         text = st.sidebar.text_input("Text")
+        text_position = st.sidebar.selectbox("Text Position", ["Top", "Bottom"])
         position_x = st.sidebar.slider("Position X", 0, 1000, 500)
         position_y = st.sidebar.slider("Position Y", 0, 1000, 500)
         font_size = st.sidebar.slider("Font Size", 10, 100, 20)
+        font_color = st.sidebar.color_picker("Font Color", value="#FFFFFF")
+        font_thickness = st.sidebar.slider("Font Thickness", 1, 5, 2)
+        font_path = st.sidebar.text_input("Font Path", "Arial.ttf")
 
     image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
     if not image_file:
@@ -158,7 +180,9 @@ def main_loop():
 
     if add_text_checkbox and text:
         position = (position_x, position_y)
-        processed_image = add_text(processed_image, text, position, font_size)
+        text_position = text_position.lower()
+        processed_image = add_text(processed_image, text, position, font_size, font_color, font_thickness, font_path,
+                                   text_position)
 
     st.text("Original Image vs Processed Image")
 
