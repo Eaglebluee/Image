f644ffccd7ac6bb8b38d1145035197c53f30968e
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-13 15:28:47 +0300

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,21 +5,17 @@ from PIL import Image
 from streamlit_image_comparison import image_comparison
 import io
 
-
 def brighten_image(image, amount):
     img_bright = cv2.convertScaleAbs(image, beta=amount)
     return img_bright
-
 
 def blur_image(image, amount):
     blur_img = cv2.GaussianBlur(image, (11, 11), amount)
     return blur_img
 
-
 def enhance_details(img):
     hdr = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
     return hdr
-
 
 def cartoon_effect(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -29,31 +25,24 @@ def cartoon_effect(img):
     cartoon_img = cv2.bitwise_and(color, color, mask=edges)
     return cartoon_img
 
-
 def greyscale(img):
     greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return greyscale
 
-
 def sepia(img):
-    img_sepia = np.array(img, dtype=np.float64)  # converting to float to prevent loss
-    img_sepia = cv2.transform(img_sepia, np.matrix([[0.272, 0.534, 0.131],
-                                                    [0.349, 0.686, 0.168],
-                                                    [0.393, 0.769, 0.189]]))  # multiplying image with sepia matrix
-    img_sepia[np.where(img_sepia > 255)] = 255  # normalizing values greater than 255 to 255
+    img_sepia = np.array(img, dtype=np.float64)
+    img_sepia = cv2.transform(img_sepia, np.matrix([[0.272, 0.534, 0.131], [0.349, 0.686, 0.168], [0.393, 0.769, 0.189]]))
+    img_sepia[np.where(img_sepia > 255)] = 255
     img_sepia = np.array(img_sepia, dtype=np.uint8)
     return img_sepia
-
 
 def pencil_sketch_grey(img):
     sk_gray, sk_color = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.1)
     return sk_gray
 
-
 def invert(img):
     inv = cv2.bitwise_not(img)
     return inv
-
 
 def summer(img):
     summer_img = img.copy()
@@ -61,18 +50,15 @@ def summer(img):
     summer_img[..., 2] = np.clip(img[..., 2] * 0.8, 0, 255)
     return summer_img
 
-
 def winter(img):
     winter_img = img.copy()
     winter_img[..., 0] = np.clip(img[..., 0] * 0.8, 0, 255)
     winter_img[..., 2] = np.clip(img[..., 2] * 1.2, 0, 255)
     return winter_img
 
-
 def save_image(image, filename):
     image_pil = Image.fromarray(image)
     image_pil.save(filename)
-
 
 def main_loop():
     st.title("Image Editor")
@@ -101,15 +87,17 @@ def main_loop():
     detect_faces = st.sidebar.checkbox('Detect Faces')
 
     if detect_faces:
-        face_filters = {
-            "Filter 1": "Apply Filter 1 to the detected faces",
-            "Filter 2": "Apply Filter 2 to the detected faces",
-            "Filter 3": "Apply Filter 3 to the detected faces",
-            "Filter 4": "Apply Filter 4 to the detected faces"
-        }
-        selected_face_filter = st.sidebar.selectbox("Face Filters", list(face_filters.keys()), format_func=lambda x: x)
-        face_filter_tooltip = face_filters[selected_face_filter]
-        st.sidebar.text(face_filter_tooltip)
+        face_filter_applied = st.checkbox('Apply Face Filter')
+        if face_filter_applied:
+            face_filters = {
+                "Filter 1": "Apply Filter 1 to the detected faces",
+                "Filter 2": "Apply Filter 2 to the detected faces",
+                "Filter 3": "Apply Filter 3 to the detected faces",
+                "Filter 4": "Apply Filter 4 to the detected faces"
+            }
+            selected_face_filter = st.selectbox("Face Filters", list(face_filters.keys()), format_func=lambda x: x)
+            face_filter_tooltip = face_filters[selected_face_filter]
+            st.text(face_filter_tooltip)
 
     image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
     if not image_file:
@@ -142,9 +130,9 @@ def main_loop():
         processed_image = enhance_details(processed_image)
 
     if detect_faces:
-        processed_image, faces = detect_and_draw_faces(processed_image)
-        if selected_face_filter:
-            processed_image = apply_face_filter(processed_image, faces, selected_face_filter)
+        processed_image, faces, eyes = detect_and_draw_faces(processed_image)
+        if face_filter_applied and selected_face_filter:
+            processed_image = apply_face_filter(processed_image, faces, eyes, selected_face_filter)
 
     st.text("Original Image vs Processed Image")
 
@@ -176,10 +164,10 @@ def detect_and_draw_faces(image):
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-    return image, faces
+    return image, faces, eyes
 
 
-def apply_face_filter(image, faces, selected_filter):
+def apply_face_filter(image, faces, eyes, selected_filter):
     if selected_filter == "Filter 1":
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
